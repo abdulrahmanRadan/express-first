fef2b894b870f54f57fa65146361df65930f8480
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-25 01:06:54 +0300

    Implementing Basic Routing

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// تعريف المسار / لتعامل مع POST
-app.post("/user", (req, res, next) => {
-  res.send("<h1>User: " + req.body.username + "</h1>");
-});
-
-// تعريف المسار / لتعامل مع GET
-app.get("/", (req, res, next) => {
-  res.send(
-    '<form action="/user" method="POST"><input type="text" name="username" ><button type="submit">Create User </button></form>'
-  );
-});
-
+app.use(placesRoutes);
 app.listen(5000);
