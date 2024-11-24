6cb2bbdfbd09dd49e8d529de45a8e9c7ead113a8
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-25 00:43:45 +0300

    Understanding the Advantages of Express

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// تعريف المسار / لتعامل مع POST
+app.post("/user", (req, res, next) => {
+  res.send("<h1>User: " + req.body.username + "</h1>");
+});
+
+// تعريف المسار / لتعامل مع GET
+app.get("/", (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="username" ><button type="submit">Create User </button></form>'
+  );
+});
 
 app.listen(5000);
